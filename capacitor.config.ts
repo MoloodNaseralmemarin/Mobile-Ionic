2bbbd93e6e73c0122eea04ba5e5bab5a9a3eb7f2
Admin <MoloodNaseralmemarin@gmail.com>
2023-04-09 14:05:53 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Dr.Shop2City-Mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
